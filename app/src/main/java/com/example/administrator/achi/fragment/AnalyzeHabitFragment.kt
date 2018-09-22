a9e2beeff0bdcc3181b690d03bc28aaedf45485a
Juryun <id01096883446@gmail.com>
2018-09-23 02:46:51 +0900

    프레그먼트 페이지 생성

---
+++ app/src/main/java/com/example/administrator/achi/fragment/AnalyzeHabitFragment.kt
@@ -0,0 +1,41 @@
+package com.example.administrator.achi.fragment
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.administrator.achi.R
+
+class AnalyzeHabitFragment : Fragment(){
+    private val TAG = "AnalyzeHabitFragment"
+    private var thisView: View? = null
+
+    override fun onCreate(savedInstanceState: Bundle?){
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onResume(){
+        super.onResume()
+        Log.d(TAG, "onResume()")
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        Log.d(TAG, "thisView == null")
+
+        if(thisView == null) {
+            thisView = inflater.inflate(R.layout.fragment_analyzehabit, container, false)
+
+        }
+        return thisView
+
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = AnalyzeHabitFragment()
+    }
+
+}
