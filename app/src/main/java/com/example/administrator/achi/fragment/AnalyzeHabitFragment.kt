0a4f4dcc1898bcd561676610df545aaa150cc11a
yoosunnam <ysnam19@naver.com>
2018-10-01 21:44:38 +0900

    Modified design

--- app/src/main/java/com/example/administrator/achi/fragment/AnalyzeHabitFragment.kt
+++ app/src/main/java/com/example/administrator/achi/fragment/AnalyzeHabitFragment.kt
@@ -70,7 +70,7 @@ class AnalyzeHabitFragment : Fragment(){
         val random = Random()
         val num = random.nextInt(101)
 
-        tv_weeklyScore.text = num.toString() + " / 100점"
+        tv_weeklyScore.text = num.toString() + " / 100 점"
     }
 
     fun getDate(){
