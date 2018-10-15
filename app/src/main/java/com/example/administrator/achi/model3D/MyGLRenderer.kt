6a5911c68101e7ab3f4c442c6e81fecc21c65682
Sujin Kim <gfsusan@naver.com>
2018-10-15 12:40:15 +0900

    Added files for 3d modeling

---
+++ app/src/main/java/com/example/administrator/achi/model3D/MyGLRenderer.kt
@@ -0,0 +1,73 @@
+package com.susan.openglpractice
+
+import android.opengl.GLES20
+import android.opengl.GLSurfaceView
+import android.opengl.Matrix
+import android.os.SystemClock
+import javax.microedition.khronos.egl.EGLConfig
+import javax.microedition.khronos.opengles.GL10
+
+class MyGLRenderer : GLSurfaceView.Renderer {
+
+    private lateinit var mTriangle: Triangle
+
+    var angle: Float = 0f
+
+    override fun onSurfaceCreated(unused: GL10, config: EGLConfig?) {
+        // Set the background frame color
+        GLES20.glClearColor(0.0f, 0.0f, 0.0f, 1.0f)
+
+        // initialize a triangle
+        mTriangle = Triangle()
+    }
+
+    private val mRotationMatrix = FloatArray(16)
+
+    override fun onDrawFrame(gl: GL10) {
+        val scratch = FloatArray(16)
+
+        GLES20.glClear(GLES20.GL_COLOR_BUFFER_BIT)
+
+
+        // Set the camera position (View matrix)
+        Matrix.setLookAtM(mViewMatrix, 0, 0f, 0f, -3f, 0f, 0f, 0f, 0f, 1.0f, 0.0f)
+
+        // Calculate the projection and view transformation
+        Matrix.multiplyMM(mMVPMatrix, 0, mProjectionMatrix, 0, mViewMatrix, 0)
+
+        /*
+        // Draw shape
+        mTriangle.draw(mMVPMatrix)
+        // mTriangle.draw()
+*/
+
+        // Create a rotation transformation for the triangle
+//        val time = SystemClock.uptimeMillis() % 4000L
+//        val angle = 0.090f * time.toInt()
+        Matrix.setRotateM(mRotationMatrix, 0, angle, 0f, 0f, -1.0f)
+
+        // Combine the rotation matrix with the projection and camera view
+        // Note that the mMVPMatrix factor *must be first* in order
+        // for the matrix multiplication product to be correct.
+        Matrix.multiplyMM(scratch, 0, mMVPMatrix, 0, mRotationMatrix, 0)
+
+        // Draw triangle
+        mTriangle.draw(scratch)
+    }
+
+    // mMVPMatrix is an abbreviation for "Model View Projection Matrix"
+    private val mMVPMatrix = FloatArray(16)
+    private val mProjectionMatrix = FloatArray(16)
+    private val mViewMatrix = FloatArray(16)
+
+    override fun onSurfaceChanged(unused: GL10, width: Int, height: Int) {
+        GLES20.glViewport(0, 0, width, height)
+
+        val ratio: Float = width.toFloat() / height.toFloat()
+
+        // this projection matrix is applied to object coordinates
+        // in the onDrawFrame() method
+        Matrix.frustumM(mProjectionMatrix, 0, -ratio, ratio, -1f, 1f, 3f, 7f)
+    }
+
+}
