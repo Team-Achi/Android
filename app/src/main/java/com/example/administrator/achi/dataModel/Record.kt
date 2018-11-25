159b73db59c310008713efca48527c45c80b4a9d
Sujin Kim <gfsusan@naver.com>
2018-11-25 22:11:38 +0900

    Made instance() method static

--- app/src/main/java/com/example/administrator/achi/dataModel/Record.kt
+++ app/src/main/java/com/example/administrator/achi/dataModel/Record.kt
@@ -6,6 +6,39 @@ import java.time.LocalDateTime
 import java.util.*
 
 class Record {
+    companion object {
+        fun instance(data: String) : Record? {
+            val dataArray:List<String> = data.split("/")
+            if (dataArray.size != 8) {
+                return null
+            }
+            val date = LocalDateTime.parse(dataArray[ContentIndex.DATE.ordinal])
+            val duration = dataArray[ContentIndex.DURATION.ordinal].toDouble()
+
+            val sCPT:String = data[ContentIndex.CNT_PER_TOOTH.ordinal].toString()
+            val temp = sCPT.split(",")
+
+            val cnt_per_tooth = Array<Int>(50, {0})
+            var ctr = 0
+            for (index in Analyzer.TEETH_INDICES) {
+                cnt_per_tooth[index] = temp[ctr].toInt()
+            }
+
+            val sST = data[ContentIndex.SECTION_TIME.ordinal].toString()
+            val temp2 = sST.split(",")
+            val section_time = Array<Int>(6, {0})
+            ctr = 0
+            for (i in temp2) {
+                section_time[ctr] = i.toInt()
+            }
+            val high_pressure = data[ContentIndex.HIGH_PRESSURE.ordinal].toInt()
+            val low_pressure = data[ContentIndex.LOW_PRESSURE.ordinal].toInt()
+            val score: Int = data[ContentIndex.SCORE.ordinal].toInt()
+            val comment :String = data[ContentIndex.COMMENT.ordinal].toString()
+
+            return Record(date, duration, cnt_per_tooth, section_time, high_pressure, low_pressure, score, comment)
+        }
+    }
     val TAG = "RECORD"
 
     var date : LocalDateTime = LocalDateTime.now()
@@ -40,37 +73,7 @@ class Record {
         // parse string data
     }
 
-    fun instance(data: String) : Record? {
-        val dataArray:List<String> = data.split("/")
-        if (dataArray.size != 8) {
-            return null
-        }
-        val date = LocalDateTime.parse(dataArray[ContentIndex.DATE.ordinal])
-        val duration = dataArray[ContentIndex.DURATION.ordinal].toDouble()
 
-        val sCPT:String = data[ContentIndex.CNT_PER_TOOTH.ordinal].toString()
-        val temp = sCPT.split(",")
-
-        val cnt_per_tooth = Array<Int>(50, {0})
-        var ctr = 0
-        for (index in Analyzer.TEETH_INDICES) {
-            cnt_per_tooth[index] = temp[ctr].toInt()
-        }
-
-        val sST = data[ContentIndex.SECTION_TIME.ordinal].toString()
-        val temp2 = sST.split(",")
-        val section_time = Array<Int>(6, {0})
-        ctr = 0
-        for (i in temp2) {
-            section_time[ctr] = i.toInt()
-        }
-        val high_pressure = data[ContentIndex.HIGH_PRESSURE.ordinal].toInt()
-        val low_pressure = data[ContentIndex.LOW_PRESSURE.ordinal].toInt()
-        val score: Int = data[ContentIndex.SCORE.ordinal].toInt()
-        val comment :String = data[ContentIndex.COMMENT.ordinal].toString()
-
-        return Record(date, duration, cnt_per_tooth, section_time, high_pressure, low_pressure, score, comment)
-    }
 
     override fun toString() : String {
         var s = String()
