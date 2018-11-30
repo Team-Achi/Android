940cd49c353a1aa1bcddab56dfa9898ee3710219
Sujin Kim <gfsusan@naver.com>
2018-11-30 18:34:19 +0900

    Record class to data class

--- app/src/main/java/com/example/administrator/achi/dataModel/Record.kt
+++ app/src/main/java/com/example/administrator/achi/dataModel/Record.kt
@@ -3,34 +3,7 @@ package com.example.administrator.achi.dataModel
 import java.time.LocalDateTime
 import java.util.*
 
-class Record {
-    var date : LocalDateTime
-        private set
-    var duration : Double = 0.0
-        private set
-    var cnt_per_tooth : Array<Int> = Array<Int>(50,{0})
-        private set
-    var section_time : Array<Int> = Array<Int>(6,{0})
-        private set
-    var high_pressure : Int = 0
-        private set
-    var low_pressure : Int = 0
-        private set
-    var score : Int = 100
-        private set
-    var comment : String = "여기는 양치에 대한 코멘트입니당"
-        private set
-
-    constructor(date : LocalDateTime, time : Double, cpt : Array<Int>, st : Array<Int>, hp : Int, lp : Int, score : Int, comment : String) {
-        this.date = date
-        this.duration = time
-        this.cnt_per_tooth = cpt
-        this.section_time = st
-        this.high_pressure = hp
-        this.low_pressure = lp
-        this.score = score
-        this.comment = comment
-    }
+data class Record(var date : LocalDateTime, var duration : Double, var cnt_per_tooth : Array<Int>, var section_time : Array<Int>, var high_pressure: Int, var low_pressure : Int, var score : Int, var comment : String) {
 
 
     // just for test
@@ -39,6 +12,7 @@ class Record {
         println(this.date.toString())
         println(this.duration)
         println(this.cnt_per_tooth[21])
+        println(this.section_time)
         println(this.high_pressure)
         println(this.low_pressure)
         println(this.score)
--- app/src/main/java/com/example/administrator/achi/fragment/MonitoringFragment.kt
+++ app/src/main/java/com/example/administrator/achi/fragment/MonitoringFragment.kt
@@ -73,6 +73,7 @@ class MonitoringFragment : Fragment(){
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
 
+        curState = RUN
         this.paramUri = Uri.parse("nothing")
     }
 
